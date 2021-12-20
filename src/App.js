ff67060aff256357a1d7729b94da98284cf7ae2c
Eduardo Escobar <eduarddo.escobar@gmail.com>
2021-12-20 13:37:04 -0500

    Added the footer and font to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 // import { useEffect, useState } from 'react';
+import styled from "styled-components";
 import './App.css';
 
-import Header from './components/Header';
+import Header from './components/constants/Header';
+import Footer from './components/constants/Footer';
 import Home from './components/Home';
 import Potluck from './components/Potluck';
 import Invites from './components/Invites';
@@ -11,12 +13,31 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Logout from './components/Logout';
 
+const StyledDiv = styled.div`
+  font-family: 'Caveat', cursive;
+  h1{
+    font-size: 4rem;
+  }
+
+  Footer, Header nav{
+    padding: 0 3%;
+    background-color: #FFCC96;
+    font-size: 2.5rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  a {
+    color: #FFFFFF;
+  }
+`
+
 
 function App() {
   const [loggedOut, setLoggedOut] = useState(true);
 
   return (
-    <div>
+    <StyledDiv>
       <Header loggedOut={loggedOut}/>
       <Routes>
         <Route exact path='/' element={<Home/>} />
@@ -26,7 +47,8 @@ function App() {
         <Route exact path='/signup' element={<Signup/>}/>
         <Route exact path='/logout' element={<Logout setLoggedOut={setLoggedOut}/>}/>
       </Routes>
-    </div>
+      <Footer />
+    </StyledDiv>
   );
 }
 
