e7dd3bbf39df356490d67622f066e91305963828
Eduardo Escobar <eduarddo.escobar@gmail.com>
2021-12-21 13:19:35 -0500

    Added some files for the faq and contact pages

---
+++ src/components/pages/faq/Question.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledQuestion = styled.div`
+
+`
+
+const Question = (props) => {
+    const {question, answer} = props;
+
+    return (
+        <StyledQuestion>
+        </StyledQuestion>
+    )
+}
+
+export default Question;
