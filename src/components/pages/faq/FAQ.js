3a3a71542e6ba149aaf727861a0c2dcc6750a17e
Eduardo Escobar <eduarddo.escobar@gmail.com>
2021-12-21 13:57:56 -0500

    Made a Question component and added a How do you sign up quesiton in FAQ

--- src/components/pages/faq/FAQ.js
+++ src/components/pages/faq/FAQ.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import Question from './Question';
+
+const StyledFAQ = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 2% 0;
+`
 
 const FAQ = (props) => {
     return (
-        <>
-        </>
+        <StyledFAQ>
+            <Question question={'How do you sign up?'} answer={'You go to our sign up page and enter your information!'} link={'/signup'} linkText={'Sign up page'} />
+            <Question question={'How do you sign up?'} answer={'You go to our sign up page and enter your information!'} />
+        </StyledFAQ>
     )
 }
 
--- src/components/pages/faq/Question.js
+++ src/components/pages/faq/Question.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
 
 const StyledQuestion = styled.div`
+    margin-top: .5%;
+    border: 2px solid black;
+    border-radius: 10px;
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
 
+    button{
+        border:none;
+        background-color: transparent;
+        width: 4%;
+    }
+    button:hover{
+        cursor: pointer;
+    }
+`
+
+const StyledAnswer = styled.div`
+    border: 2px solid black;
+    border-radius: 10px;
+    background-color: #FFCC96;
+    text-shadow: 2px 2px 2px black;
+    color: white;
+    height: auto;
+    width: 60%;
+    font-size: 1rem;
+
+    a{
+        margin: 4% 0;
+        font-size: 1.2rem;
+        text-shadow: none;
+        border-bottom: none;
+        
+    }
 `
 
 const Question = (props) => {
-    const {question, answer} = props;
+    const {question, answer, link, linkText} = props;
+    const [open, setOpen] = useState(false);
 
     return (
-        <StyledQuestion>
-        </StyledQuestion>
+        <>
+            <StyledQuestion onClick={() => setOpen(!open)}>
+                <h3>{question}</h3>
+                <button>{open ? <>&#9650;</>: <>&#9660;</>}</button>
+            </StyledQuestion>
+            {open && (
+                <StyledAnswer>
+                    <p>{answer}</p>
+                    {link && <Link to={link}>{linkText}</Link>}
+                </StyledAnswer>
+            )}
+        </>
     )
 }
 
